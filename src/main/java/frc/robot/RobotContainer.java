bf130fc3ea79076d5e40272566d4ef052be2538c
tiwi28 <tiwiasaba2008@gmail.com>
2025-03-05 20:21:30 -0500

    Reduced robot drive and steer speeds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -60,7 +60,7 @@ public class RobotContainer {
     }
 
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
+    private double MaxAngularRate = RotationsPerSecond.of(0.5).in(RadiansPerSecond); // Was originally 3/4 of a rotation per second
                                                                                       // max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */ // TODO: move to a drive command
